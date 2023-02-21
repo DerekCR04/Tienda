93e96be42629fe5ec92e96fbb5a1865df2eb7970
derek <derek@DESKTOP-R2ABRMM>
2023-02-20 22:51:11 -0600

    Finalizacion de la Clase 5

--- src/main/java/com/Tienda/controller/IndexController.java
+++ src/main/java/com/Tienda/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.Tienda.controller;
 
 import com.Tienda.dao.ClienteDao;
 import com.Tienda.domain.Cliente;
+import com.Tienda.service.ClienteService;
 import java.util.Arrays;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 /**
  *
  * @author derek
@@ -19,24 +21,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
     
     @Autowired
-    ClienteDao clienteDao;
+    ClienteService clienteService;
     
     @GetMapping("/")
     public String inicio(Model model) {
         log.info("Ahora utilizando MVC");
-        /*String mensaje="Estamos en semana 4!";
-        model.addAttribute("Mensaje", mensaje);
         
-        Cliente cliente = new Cliente("Derek", "Castillo","dcastillo70903@ufide.ac.cr", "85324288");
-        Cliente cliente2 = new Cliente("Joel", "Quiros","jquiros70903@ufide.ac.cr", "85324288");
-        //model.addAttribute("cliente", cliente);
-        
-        List<Cliente> clientes = Arrays.asList(cliente, cliente2);
-        model.addAttribute("clientes", clientes);*/
-        var clientes=clienteDao.findAll();
+        var clientes=clienteService.getCliente();
+        //var clientes= Arrays.asList();
         model.addAttribute("clientes", clientes);
 
         return "index";
     }
     
+    @GetMapping("/nuevoCliente")
+    public String nuevoCliente(Cliente cliente){
+        return "modificarCliente";
+    }
+    @PostMapping("/guardarCliente")
+    public String guardarCliente(Cliente cliente){
+        clienteService.save(cliente);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/modificarCliente/{idCliente}")
+    public String modificarCliente(Cliente cliente, Model model){
+        cliente = clienteService.getCliente(cliente);
+        model.addAttribute("cliente", cliente);
+        return "modificarCliente";
+    }
+    
+    @GetMapping("/eliminarCliente/idCliente")
+    public String eliminarCliente(Cliente cliente){
+        clienteService.delete(cliente);
+        return "redirect:/";
+    }
 }
---
+++ src/main/java/com/Tienda/service/ClienteService.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.Tienda.service;
+
+import com.Tienda.domain.Cliente;
+import java.util.List;
+
+/**
+ *
+ * @author derek
+ */
+public interface ClienteService {
+    
+    public List<Cliente> getCliente();
+    public Cliente getCliente(Cliente cliente);
+    public void save(Cliente cliente);
+    public void delete(Cliente cliente);
+    
+    
+}
